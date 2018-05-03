110301495503e01dabfac34e9ade32b544ddb610
physicaldog <monkey891120@63.com>
2018-05-03 08:32:40 +0800

    4

--- js/RunningStatus.js
+++ js/RunningStatus.js
@@ -3,7 +3,7 @@ $(function(){
 	sysInfo();
 	doWork();
 	deviceInfo();
-	setInterval('sysInfo()',60);
+	//setInterval('sysInfo()',60);
 	setInterval('doWork()',10000);
 });
 function doWork(){
@@ -12,7 +12,7 @@ function doWork(){
 	LANStatus();
 }
 //系统信息
-
+var totalTime = 0;
 function sysInfo(){
 	$.ajax({
 		type:"get",
@@ -20,10 +20,48 @@ function sysInfo(){
 		async:true,
 		dataType:'json',
 		success:function(data){
-			$('#uptime').text(data.uptime);
+		totalTime = parseInt(data.uptime);
+			setInterval('jisuan()',1000);
+			
+			
 		}
 	});
 }
+function jisuan(){
+	totalTime = totalTime+1;
+	siteTime(totalTime)
+}
+function siteTime(diff){
+//      window.setTimeout("siteTime()", 1000);
+        var seconds = 1;
+        var minutes = seconds * 60;
+        var hours = minutes * 60;
+        var days = hours * 24;
+        var years = days * 365;
+        /*var today = new Date();
+        var todayYear = today.getFullYear();
+        var todayMonth = today.getMonth()+1;
+        var todayDate = today.getDate();
+        var todayHour = today.getHours();
+        var todayMinute = today.getMinutes();
+        var todaySecond = today.getSeconds();*/
+        /* Date.UTC() -- 返回date对象距世界标准时间(UTC)1970年1月1日午夜之间的毫秒数(时间戳)
+        year - 作为date对象的年份，为4位年份值
+        month - 0-11之间的整数，做为date对象的月份
+        day - 1-31之间的整数，做为date对象的天数
+        hours - 0(午夜24点)-23之间的整数，做为date对象的小时数
+        minutes - 0-59之间的整数，做为date对象的分钟数
+        seconds - 0-59之间的整数，做为date对象的秒数
+        microseconds - 0-999之间的整数，做为date对象的毫秒数 */
+        /*var t1 = Date.UTC(2016,12,01,00,00,00); //北京时间2016-12-1 00:00:00
+        var t2 = Date.UTC(todayYear,todayMonth,todayDate,todayHour,todayMinute,todaySecond);*/
+        var diffYears = Math.floor(diff/years);
+        var diffDays = Math.floor((diff/days)-diffYears*365);
+        var diffHours = Math.floor((diff-(diffYears*365+diffDays)*days)/hours);
+        var diffMinutes = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours)/minutes);
+        var diffSeconds = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours-diffMinutes*minutes)/seconds);
+       $('#uptime').text(" 已运行"+diffYears+" 年 "+diffDays+" 天 "+diffHours+" 小时 "+diffMinutes+" 分钟 "+diffSeconds+" 秒");
+    }
 //设备信息
 function deviceInfo(){
 	$.ajax({
